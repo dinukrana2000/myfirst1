b27b90e94a7bf5f4715892c5ec4ea96387e54388
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-08 11:55:25 +0530

    Revert "sout"
    This reverts commit c6a81ba9bfaba699491fca69d1799062888284e8.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,10 +11,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		System.out.println("changes madeee");
-
-		System.out.println("hello world");
-
 	}
 
 	@Bean
